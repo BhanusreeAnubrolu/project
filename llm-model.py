5b89ebaf44e147139dfcdab02a5292a4edbe7c4e
Priyanka Ippala <priyankaippala2003@gmail.com>
2024-12-10 21:37:05 +0530

    added llm-model

---
+++ llm-model.py
@@ -0,0 +1,32 @@
+import google.generativeai as genai
+
+def llm_response(api_key,doc_data):
+    genai.configure(api_key=api_key)
+
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    
+    prompt = doc_data + "read this and give some keywords from it"
+    response = model.generate_content(prompt)
+    print(response.text)
+    
+    
+def read_and_parse_document(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return file.read()
+    except UnicodeDecodeError:
+        print("UTF-8 encoding failed. Trying with ISO-8859-1...")
+        try:
+            with open(file_path, 'r', encoding='iso-8859-1') as file:
+                return file.read()
+        except Exception as e:
+            print(f"Failed to read file: {e}")
+            return None
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        return None
+
+
+API_KEY = "AIzaSyD0DwkA-nIAh1MANQc6xjgXFql_cXvOwXw"
+doc_data = read_and_parse_document("sample.pdf")
+llm_response(API_KEY,doc_data)
